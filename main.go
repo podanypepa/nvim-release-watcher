6204abf0b35e5113377f091270f65833fffab5b4
Josef Podany <podany.pepa@gmail.com>
2023-09-13 14:19:07 +0200

    flow in main func

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 // Package main _
 package main
 
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
 const (
 	// DevelopmentPage is URL of web page with basic prerelease build
 	DevelopmentPage = "https://github.com/neovim/neovim/releases"
@@ -11,4 +17,39 @@ const (
 )
 
 func main() {
+	version, createdTime := "", ""
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		var err error
+		version, err = getVersion()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		var err error
+		createdTime, err = getReleaseFileTime()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	wg.Wait()
+
+	fmt.Println("version:    ", version)
+	fmt.Println("createdTime:", createdTime)
+}
+
+func getVersion() (string, error) {
+	return "", nil
+}
+
+func getReleaseFileTime() (string, error) {
+	return "", nil
 }
