47ecf60026e2fda581065b2c36b77dc192a8e955
Josef Podany <podany.pepa@gmail.com>
2023-09-13 14:40:39 +0200

    getversion info

--- getversion.go
+++ getversion.go
@@ -1,10 +1,27 @@
 package main
 
+import (
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
 const (
 	// DevelopmentPage is URL of web page with basic prerelease build
 	DevelopmentPage = "https://github.com/neovim/neovim/releases"
 )
 
 func getVersion() (string, error) {
-	return "", nil
+	c := colly.NewCollector()
+	version := ""
+	c.OnHTML("code", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Text, "NVIM") && version == "" {
+			version = e.Text
+			info := strings.Split(e.Text, "\n")
+			info = strings.Split(info[0], " ")
+			version = info[1]
+		}
+	})
+	c.Visit(DevelopmentPage)
+	return version, nil
 }
