9bdf88afbac3d64aa5de303032130b165331e699
Josef Podany <podany.pepa@gmail.com>
2023-09-13 16:26:11 +0200

    remove unused var

--- getreleasetime.go
+++ getreleasetime.go
@@ -7,8 +7,6 @@ import (
 const (
 	// AssetsPage is URL of web page with detail info about build files
 	AssetsPage = "https://github.com/neovim/neovim/releases/expanded_assets/nightly"
-	// NvimFile is required release file
-	NvimFile = "nvim-macos.tar.gz"
 )
 
 func getReleaseFileTime() (string, error) {
