9ff904033492ea84708d5783230b8935f635a667
Josef Podany <podany.pepa@gmail.com>
2023-09-13 14:22:11 +0200

    split funcs to separate files

---
+++ getversion.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	// DevelopmentPage is URL of web page with basic prerelease build
+	DevelopmentPage = "https://github.com/neovim/neovim/releases"
+)
+
+func getVersion() (string, error) {
+	return "", nil
+}
--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 	"sync"
 )
 
-const (
-	// DevelopmentPage is URL of web page with basic prerelease build
-	DevelopmentPage = "https://github.com/neovim/neovim/releases"
-	// AssetsPage is URL of web page with detail info about build files
-	AssetsPage = "https://github.com/neovim/neovim/releases/expanded_assets/nightly"
-	// NvimFile is required release file
-	NvimFile = "nvim-macos.tar.gz"
-)
 
 func main() {
 	version, createdTime := "", ""
@@ -44,12 +36,4 @@ func main() {
 
 	fmt.Println("version:    ", version)
 	fmt.Println("createdTime:", createdTime)
-}
-
-func getVersion() (string, error) {
-	return "", nil
-}
-
-func getReleaseFileTime() (string, error) {
-	return "", nil
 }
