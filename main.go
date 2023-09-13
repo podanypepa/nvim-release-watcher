092d747ad5c1f67f7067cd6f28cc331a1451161a
Josef Podany <podany.pepa@gmail.com>
2023-09-13 14:08:13 +0200

    add basic neovim page address

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 // Package main _
 package main
 
-func main() {
+const (
+	// DevelopmentPage is URL of web page with basic prerelease build
+	DevelopmentPage = "https://github.com/neovim/neovim/releases"
+	// AssetsPage is URL of web page with detail info about build files
+	AssetsPage = "https://github.com/neovim/neovim/releases/expanded_assets/nightly"
+	// NvimFile is required release file
+	NvimFile = "nvim-macos.tar.gz"
+)
 
+func main() {
 }
